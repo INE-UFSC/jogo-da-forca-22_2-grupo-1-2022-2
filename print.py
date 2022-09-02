c89fe05e1eca0e1d5cf4a485923d6b7572436c4b
joao-poc <joaopedrocarvalho1973@gmail.com>
2022-09-02 14:10:13 -0300

    print corrigido com a palavra secreta

--- print.py
+++ print.py
@@ -1,8 +1,8 @@
-def print_forca(vidas=6, letras_digitadas=None, palavra_secreta=None, letra_escolhida=None):
-	#letra_escolhida vem da função pedir_letra(), uso esse argumento pra ir completando a palavra.
+def print_forca(palavra_secreta="", letras_testadas={}, vidas=6):
+   #Printa os estados do boneco e completa a palavra
 	#o índice indica o número de vidas restantes
-	#letras_digitadas vem do set letras_testadas
-	estados = ["""   _______
+	#letras_testadas vem do set letras_testadas
+   estados = ["""   _______
    /	  I 
    I	  I
    I	(ಥ_ಥ)
@@ -67,25 +67,27 @@ def print_forca(vidas=6, letras_digitadas=None, palavra_secreta=None, letra_esco
    I	        	       
    I
 ==================="""]
-	""""""
-	if vidas == 6:
-		print(estados[6])
-	elif vidas == 5:
-		print(estados[5])
-	elif vidas == 4:
-		print(estados[4])
-	elif vidas == 3:
-		print(estados[3])
-	elif vidas == 2:
-		print(estados[2])
-	elif vidas == 1:
-		print(estados[1])
-	else:
-		print(estados[0])
-	letras_digitadas = list(letras_digitadas)
-	letras_digitadas.sort()
+   if vidas == 6:
+      print(estados[6])
+   elif vidas == 5:
+      print(estados[5])
+   elif vidas == 4:
+      print(estados[4])
+   elif vidas == 3:
+      print(estados[3])
+   elif vidas == 2:
+      print(estados[2])
+   elif vidas == 1:
+      print(estados[1])
+   else:
+      print(estados[0])
+   letras_testadas = list(letras_testadas)
+   letras_testadas.sort()
+   print("A palavra secreta é:", end= ' ')
+   print("".join(ch if ch in letras_testadas else "*" for ch in palavra_secreta))
+   print("As letras escolhidas até agora foram:", end= ' ')
+   for letra in letras_testadas:
+      print(letra.upper(), end= ' ')
 
-	print('As letras testadas foram:', end= ' ')
-	for letra in letras_digitadas:
-		print(letra, end= ' ')
-	#palavra_secreta_escondida = len(palavra_secreta) * '
+
+
